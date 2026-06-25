a53dc06ad3b465dbab43b1e21f928ecb1b3b23f4
patthesparrowking <paddyswaghetti@gmail.com>
2026-06-25 13:52:40 +0200

    LibraryUpdate

--- js/library.js
+++ js/library.js
@@ -1,10 +1,5 @@
 export const svgLibrary = [
   {
-    id: "Calciumchlorid-Lösung",
-    label: "Calciumchlorid-Lösung",
-    path: "assets/svg/Calciumchlorid-Lösung.svg"
-  },
-  {
     id: "Daphnia_Magna",
     label: "Daphnia Magna",
     path: "assets/svg/Daphnia_Magna.svg"
@@ -15,115 +10,9 @@ export const svgLibrary = [
     path: "assets/svg/Daphnien_Becherglas.svg"
   },
   {
-    id: "Deckgläschen",
-    label: "Deckgläschen",
-    path: "assets/svg/Deckgläschen.svg"
-  },
-  {
-    id: "Dichteflasche",
-    label: "Dichteflasche",
-    path: "assets/svg/Dichteflasche.svg"
-  },
-  {
-    id: "Eppendorf-Pipette",
-    label: "Eppendorf-Pipette",
-    path: "assets/svg/Eppendorf-Pipette.svg"
-  },
-  {
-    id: "Ethanol_Gelb",
-    label: "Ethanol Gelb",
-    path: "assets/svg/Ethanol_Gelb.svg"
-  },
-  {
-    id: "Holzspieß",
-    label: "Holzspieß",
-    path: "assets/svg/Holzspieß.svg"
-  },
-  {
-    id: "Kunststoff-Granulat",
-    label: "Kunststoff-Granulat",
-    path: "assets/svg/Kunststoff-Granulat.svg"
-  },
-  {
-    id: "Objektträger_Deckgläschen",
-    label: "Objektträger mit Deckgläschen",
-    path: "assets/svg/Objektträger_Deckgläschen.svg"
-  },
-  {
-    id: "Objektträger",
-    label: "Objektträger",
-    path: "assets/svg/Objektträger.svg"
-  },
-  {
-    id: "Petrischale_Leer",
-    label: "Petrischale (Leer)",
-    path: "assets/svg/Petrischale_Leer.svg"
-  },
-  {
-    id: "Petrischale_Wasser",
-    label: "Petrischale (Wasser)",
-    path: "assets/svg/Petrischale_Wasser.svg"
-  },
-  // {
-  //   id: "Petrischale_Amöben",
-  //   label: "Petrischale (Amöben)",
-  //   path: "assets/svg/Petrischale_Amöbe.svg"
-  // },
-  {
-    id: "Pinsel",
-    label: "Pinsel",
-    path: "assets/svg/Pinsel.svg"
-  },
-  {
-    id: "Pinzette",
-    label: "Pinzette",
-    path: "assets/svg/Pinzette.svg"
-  },
-  {
-    id: "Pipette",
-    label: "Pipette",
-    path: "assets/svg/Pipette.svg"
-  },
-
-  {
-    id: "Präparat_Glanzwurm",
-    label: "Präparat (Glanzwurm)",
-    path: "assets/svg/Präparat_Glanzwurm.svg"
-  },
-  {
-    id: "Reagenzglas_Leer",
-    label: "Reagenzglas (Leer)",
-    path: "assets/svg/Reagenzglas_Leer.svg"
-  },
-  {
-    id: "Reagenzglas_Paramecium",
-    label: "Reagenzglas (Paramecium)",
-    path: "assets/svg/Reagenzglas_Paramecium.svg"
-  },
-  {
-    id: "Reagenzglasständer",
-    label: "Reagenzglasständer",
-    path: "assets/svg/Reagenzglasständer.svg"
-  },
-  {
-    id: "Schnappdeckelglas_Leer",
-    label: "Schnappdeckelglas (Leer)",
-    path: "assets/svg/Schnappdeckelglas_Leer.svg"
-  },
-  {
-    id: "Schnappdeckelglas_Wasser",
-    label: "Schnappdeckelglas (Wasser)",
-    path: "assets/svg/Schnappdeckelglas_Wasser.svg"
-  },
-  {
-    id: "Schnappdeckelglas_Suspension",
-    label: "Schnappdeckelglas (Suspension)",
-    path: "assets/svg/Schnappdeckelglas_Suspension.svg"
-  },
-  {
-    id: "Schutzbrille",
-    label: "Schutzbrille (Normal)",
-    path: "assets/svg/Schutzbrille.svg"
+    id: "Taschenlampe_UV",
+    label: "Taschenlampe (UV)",
+    path: "assets/svg/Taschenlampe_UV.svg"
   },
   {
     id: "Schutzbrille_UV",
@@ -136,14 +25,19 @@ export const svgLibrary = [
     path: "assets/svg/Stativ.svg"
   },
   {
-    id: "Taschenlampe_Grünlicht",
-    label: "Taschenlampe (Grünlicht)",
-    path: "assets/svg/Taschenlampe_Grünlicht.svg"
+    id: "Stereomikroskop",
+    label: "Stereomikroskop",
+    path: "assets/svg/Stereomikroskop.svg"
   },
   {
-    id: "Taschenlampe_UV",
-    label: "Taschenlampe (UV)",
-    path: "assets/svg/Taschenlampe_UV.svg"
+    id: "Schnappdeckelglas_Suspension",
+    label: "Schnappdeckelglas (Suspension)",
+    path: "assets/svg/Schnappdeckelglas_Suspension.svg"
+  },
+  {
+    id: "Pipette",
+    label: "Pipette",
+    path: "assets/svg/Pipette.svg"
   },
   {
     id: "Uhrglas",
@@ -151,9 +45,179 @@ export const svgLibrary = [
     path: "assets/svg/Uhrglas.svg"
   },
   {
+    id: "Präparat_Glanzwurm",
+    label: "Präparat (Glanzwurm)",
+    path: "assets/svg/Präparat_Glanzwurm.svg"
+  },
+  {
+    id: "Taschenlampe_Grünlicht",
+    label: "Taschenlampe (Grünlicht)",
+    path: "assets/svg/Taschenlampe_Grünlicht.svg"
+  },
+    {
+    id: "Stereomikroskop",
+    label: "Stereomikroskop",
+    path: "assets/svg/Stereomikroskop.svg"
+  },
+  {
+    id: "Mikrotom",
+    label: "Mikrotom",
+    path: "assets/svg/Mikrotom.svg"
+  },
+  {
+    id: "Petrischale_Wasser",
+    label: "Petrischale (Wasser)",
+    path: "assets/svg/Petrischale_Wasser.svg"
+  },
+  {
+    id: "Pinzette",
+    label: "Pinzette",
+    path: "assets/svg/Pinzette.svg"
+  },
+  {
+    id: "Pinsel",
+    label: "Pinsel",
+    path: "assets/svg/Pinsel.svg"
+  },
+  {
+    id: "Objektträger",
+    label: "Objektträger",
+    path: "assets/svg/Objektträger.svg"
+  },
+    {
+    id: "Stereomikroskop",
+    label: "Stereomikroskop",
+    path: "assets/svg/Stereomikroskop.svg"
+  },
+  {
+    id: "Schnappdeckelglas_Wasser",
+    label: "Schnappdeckelglas (Wasser)",
+    path: "assets/svg/Schnappdeckelglas_Wasser.svg"
+  },
+  {
+    id: "Petrischale_Leer",
+    label: "Petrischale (Leer)",
+    path: "assets/svg/Petrischale_Leer.svg"
+  },
+  {
+    id: "Taschenlampe_Grünlicht",
+    label: "Taschenlampe (Grünlicht)",
+    path: "assets/svg/Taschenlampe_Grünlicht.svg"
+  },
+  {
+    id: "Pinzette",
+    label: "Pinzette",
+    path: "assets/svg/Pinzette.svg"
+  },
+    {
+    id: "Stereomikroskop",
+    label: "Stereomikroskop",
+    path: "assets/svg/Stereomikroskop.svg"
+  },
+  {
+    id: "Reagenzglas_Paramecium",
+    label: "Reagenzglas (Paramecium)",
+    path: "assets/svg/Reagenzglas_Paramecium.svg"
+  },
+  // {
+  //   id: "Petrischale_Amöben",
+  //   label: "Petrischale (Amöben)",
+  //   path: "assets/svg/Petrischale_Amöbe.svg"
+  // },
+  {
+    id: "Objektträger",
+    label: "Objektträger",
+    path: "assets/svg/Objektträger.svg"
+  },
+  {
+    id: "Glassplitter",
+    label: "Glassplitter",
+    path: "assets/svg/Glassplitter.svg"
+  },
+  {
+    id: "Deckgläschen",
+    label: "Deckgläschen",
+    path: "assets/svg/Deckgläschen.svg"
+  },
+  {
+    id: "Schnappdeckelglas_Suspension",
+    label: "Schnappdeckelglas (Suspension)",
+    path: "assets/svg/Schnappdeckelglas_Suspension.svg"
+  },
+  {
+    id: "Eppendorf-Pipette",
+    label: "Eppendorf-Pipette",
+    path: "assets/svg/Eppendorf-Pipette.svg"
+  },
+  {
+    id: "Petrischale_Leer",
+    label: "Petrischale (Leer)",
+    path: "assets/svg/Petrischale_Leer.svg"
+  },
+    {
+    id: "Stereomikroskop",
+    label: "Stereomikroskop",
+    path: "assets/svg/Stereomikroskop.svg"
+  },
+    {
+    id: "Stereomikroskop",
+    label: "Stereomikroskop",
+    path: "assets/svg/Stereomikroskop.svg"
+  },
+  {
+    id: "Calciumchlorid-Lösung",
+    label: "Calciumchlorid-Lösung",
+    path: "assets/svg/Calciumchlorid-Lösung.svg"
+  },
+  {
     id: "Wasser_Blau",
     label: "Wasser (Blau)",
     path: "assets/svg/Wasser_Blau.svg"
+  },
+  {
+    id: "Ethanol_Gelb",
+    label: "Ethanol Gelb",
+    path: "assets/svg/Ethanol_Gelb.svg"
+  },
+  {
+    id: "Reagenzglas_Leer",
+    label: "Reagenzglas (Leer)",
+    path: "assets/svg/Reagenzglas_Leer.svg"
+  },
+  {
+    id: "Holzspieß",
+    label: "Holzspieß",
+    path: "assets/svg/Holzspieß.svg"
+  },
+  {
+    id: "Kunststoff-Granulat",
+    label: "Kunststoff-Granulat",
+    path: "assets/svg/Kunststoff-Granulat.svg"
+  },
+  {
+    id: "Dichteflasche",
+    label: "Dichteflasche",
+    path: "assets/svg/Dichteflasche.svg"
+  },
+  {
+    id: "Objektträger_Deckgläschen",
+    label: "Objektträger mit Deckgläschen",
+    path: "assets/svg/Objektträger_Deckgläschen.svg"
+  },
+  {
+    id: "Reagenzglasständer",
+    label: "Reagenzglasständer",
+    path: "assets/svg/Reagenzglasständer.svg"
+  },
+  {
+    id: "Schnappdeckelglas_Leer",
+    label: "Schnappdeckelglas (Leer)",
+    path: "assets/svg/Schnappdeckelglas_Leer.svg"
+  },
+  {
+    id: "Schutzbrille",
+    label: "Schutzbrille (Normal)",
+    path: "assets/svg/Schutzbrille.svg"
   },
   {
     id: "Material",
